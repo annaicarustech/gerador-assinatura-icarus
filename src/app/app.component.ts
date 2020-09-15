86b6efe1b091f2500793802e3e9172c1c1a1441c
Anna Luiza Espindola <anna@icarustech.com.br>
2020-09-15 15:43:10 -0300

    assinatura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gerador-assinatura-icarus';
+export class AppComponent implements OnInit {
+  assinaturaForm
+  assinatura = false;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit() {
+    this.assinaturaForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      email: ['', Validators.required],
+      cargo: ['', Validators.required],
+      telefone: [''],
+    });
+  }
+
+  submitForm(){
+    this.assinatura = true;
+  }
+
 }
